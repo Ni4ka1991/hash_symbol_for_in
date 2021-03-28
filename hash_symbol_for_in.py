6ed8df0dcb21f5b712e95e52ed5c66a996f31176
Ni4ka1991 <astahova_nica@mail.ru>
2021-03-28 18:38:14 +0300

    Add file hash_symbol_for_in.py

--- hash_symbol_for_in.py
+++ hash_symbol_for_in.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 
+from  os import system
+
+system( "clear" )
+
+#DATA
+
+max_X = max_Y = 11
+min_X = min_Y = 1
 
 print("\n")
 
-for y in range(1, 11):
-    for x in range(1, 11):
-        if x == 1:
-            print("#", end=" ")
-        else:
-            print(".", end=" ")
-    print()
+#LOGIC
+
+
+for y in range( min_Y, max_Y ):
+
+    if(( y == min_Y ) or ( y == max_Y - 1 )):
+        
+        for x in range( min_X - 1, max_X - 1 ):
+            print("#", end = " " )
+        print()
+    
+    
+    else:
+        
+        for x in range( min_X, max_X):
+            
+            if(( x == min_X ) or (x == max_X - 1 )):
+                print("#", end=" ")
+            else:
+                print(".", end=" ")
+    
+        print()
 
 print("\n")
