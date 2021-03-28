8e3bf032112e465f275d1c31cac6cfe01bb29f0e
Veronica <79570212+Ni4ka1991@users.noreply.github.com>
2021-03-28 17:32:02 +0300

    Update hash_symbol_for_in.py

--- hash_symbol_for_in.py
+++ hash_symbol_for_in.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
 
+
+print("\n")
+
+for y in range(1, 11):
+    for x in range(1, 11):
+        if x == 1:
+            print("#", end=" ")
+        else:
+            print(".", end=" ")
+    print()
+
+print("\n")
